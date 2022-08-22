21dd74ffd81659ebba32e2c19c0d7a440b245a5a
Hamza862-Naeem <h.mhamzanaeem862@gmail.com>
2022-08-22 15:45:55 +0500

    basics file

---
+++ basics.py
@@ -0,0 +1,15 @@
+
+"input in python as cin in cpp"
+
+# age=input("What is your age?")
+
+"not optimun way above"
+
+ #  print("What is your name?")
+ #  name=input()
+
+  first=input("Enter first number :")
+  second=input("Enter second number:")
+
+  sum= var (first)+var (second)
+  print(sum)
---
+++ index.py
@@ -0,0 +1,5 @@
+first_name="Tony"
+last_name= "Stark" 
+age = 52 
+is_genius=True
+print(first_name, last_name,age,is_genius)
